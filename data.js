f22fcdf308b62975c58d3edece5d2e4ce7d8515c
farhan <mmhdfarhan08@gmail.com>
2025-02-18 09:04:31 +0700

    Update data.js

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
         const pasien = dataPasien[index];
 
         if (pasien) {
-            window.location.href = `pembayaran.html
+            window.location.href = `pembayaran.html`
         } else {
             alert("Data pasien tidak ditemukan!");
         }
